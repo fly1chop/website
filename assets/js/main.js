bfbebe0fb4e7b2ff8c2eb3e3e515d14d3f7308c9
ckim48 <ckim48@u.rochester.edu>
2023-04-08 17:19:40 +0900

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Multi - v2.2.0
-* Template URL: https://bootstrapmade.com/multi-responsive-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function ($) {
   "use strict";
 
@@ -204,9 +199,10 @@
       once: true
     });
   }
+  
   $(window).on('load', function () {
     aos_init();
   });
 
 })(jQuery);
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.2.1/chart.min.js" integrity="sha512-v3ygConQmvH0QehvQa6gSvTE2VdBZ6wkLOlmK7Mcy2mZ0ZF9saNbbk19QeaoTHdWIEiTlWmrwAL4hS8ElnGFbA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
