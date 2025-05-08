d2537e00616809cbf297b43276b04f6501127ac8
Jane Ro <janero95@gmail.com>
2025-05-08 16:32:37 +0900

    feat: mobile navbar

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,17 @@
 import 'flowbite';
+import { Dropdown } from 'flowbite';
+
+const $mobileNavbarDropdown = document.querySelector('#mobile-navbar-dropdown');
+const $mobileNavbarDropdownButton = document.querySelector('#mobile-navbar-dropdown-button');
+const $nav = document.querySelector('nav');
+const mobileNavbarDropdown = new Dropdown($mobileNavbarDropdown, $mobileNavbarDropdownButton, {
+  offsetDistance: 0,
+  onShow: () => {
+    $nav.classList.add('max-md:bg-white', 'max-md:[&_img.logo]:invert');
+    $mobileNavbarDropdownButton.classList.remove('text-white');
+  },
+  onHide: () => {
+    $nav.classList.remove('max-md:bg-white', 'max-md:[&_img.logo]:invert');
+    $mobileNavbarDropdownButton.classList.add('text-white');
+  },
+});
