1fa0570c96074949c3e01d35195374404a0d5255
Jane Ro <janero95@gmail.com>
2025-05-14 16:31:09 +0900

    refactor: dynamically render team grid items

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,10 @@
 import 'flowbite';
-import { Dropdown, Tabs } from 'flowbite';
-import { LINKS } from '../lib/constants';
+import { Dropdown } from 'flowbite';
+import { INSTRUCTORS, LINKS, MENTORS } from '../lib/constants';
+
+window.addEventListener('load', () => {
+  showCurrentTab();
+});
 
 ///////////////////
 //// Dropdown
@@ -21,11 +25,12 @@ new Dropdown($mobileNavbarDropdown, $mobileNavbarDropdownButton, {
 
 ///////////////////
 //// Tabs
-window.addEventListener('load', function () {
+const showCurrentTab = () => {
   const aboutTabs = FlowbiteInstances.getInstance('Tabs', 'tab-menu--about');
+  if (!aboutTabs) return;
   const currentTab = window.location.hash;
   if (aboutTabs && currentTab) aboutTabs.show(currentTab);
-});
+};
 
 document.querySelectorAll('.do-toggle-flip').forEach((btn) => {
   btn.addEventListener('click', () => {
@@ -41,3 +46,46 @@ Object.entries(LINKS).forEach(([name, url]) => {
     btn.setAttribute('target', '_blank');
   });
 });
+
+//////////////////////////////////////
+//////////////////////////////////////
+//// About Page
+const $mentorsGridContainer = document.getElementById('mentors-grid');
+const $instructorsGridContainer = document.getElementById('instructors-grid');
+
+const instructorsHtml = INSTRUCTORS.filter((item) => !item.isMain)
+  .map(
+    (item) => `
+  <div class="bg-zinc-50 shadow-lg overflow-hidden rounded-xs">
+    <img
+      src="${new URL('../img/team.jpg', import.meta.url)}"
+      alt="${item.name}"
+      class="aspect-square w-full" />
+    <div class="flex flex-col gap-2 p-4 text-sm">
+      <p class="text-base font-medium">${item.name}</p>
+      <p>${item.field.join()}</p>
+      <p>${item.school}</p>
+    </div>
+  </div>
+`
+  )
+  .join('');
+$instructorsGridContainer.innerHTML = instructorsHtml;
+
+const mentorsHtml = MENTORS.map(
+  (item) => `
+  <div class="bg-zinc-50 shadow-lg overflow-hidden rounded-xs">
+    <img
+      src="${new URL('../img/team.jpg', import.meta.url)}"
+      alt="${item.name}"
+      class="aspect-square w-full" />
+    <div class="flex flex-col gap-2 p-4 text-sm">
+      <p class="text-base font-medium">${item.name}</p>
+      <p>${item.field.join()}</p>
+      <p class="font-medium">${item.employment}</p>
+      <p>${item.school}</p>
+    </div>
+  </div>
+`
+).join('');
+$mentorsGridContainer.innerHTML = mentorsHtml;
