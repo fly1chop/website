0df5a56aa3d189e3d3be670ab8b4e6831d35241a
Jane Ro <janero95@gmail.com>
2025-06-25 12:04:26 +0900

    feat: project gpt brochure page

--- scripts/redirects.config.js
+++ scripts/redirects.config.js
@@ -2,6 +2,7 @@ module.exports = {
   // redirect-from: redirect-to
   '/dokdo': '/capstone/dokdo',
   '/horizon': '/capstone/horizon',
+  '/gpt': '/capstone/gpt',
   '/team': '/about#team',
   '/competition': '/courses/competition',
   '/courses': '/courses/summer',
