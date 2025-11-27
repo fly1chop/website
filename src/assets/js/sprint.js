152a5470340c46c03cac3ab02bc3264177257de4
Jane Ro <janero95@gmail.com>
2025-11-27 18:44:56 +0900

    refactor: handle null conditions

--- src/assets/js/sprint.js
+++ src/assets/js/sprint.js
@@ -13,7 +13,7 @@ const ongoingHtml = sprint
       <span class="text-5xl font-bold w-12 block text-center border-b pb-1.5 border-flamingo-500 text-zinc-300">${i + 1}</span>
       <div class="space-y-2">
         <p class="text-3xl font-semibold">${p.name}</p>
-        ${p.client && `<p class="font-medium">Client | Partner: ${p.client}</p>`}
+        ${p.client ? `<p class="font-medium">Client | Partner: ${p.client}</p>` : ''}
       </div>
       <div class="space-y-2">
         <p class="text-zinc-600 italic">Term: ${p.duration}</p>
@@ -35,22 +35,24 @@ const ongoingHtml = sprint
       <p class="leading-7 break-keep whitespace-pre-line">${p.description}</p>
       <div class="flex gap-2 flex-col sm:flex-row">
           ${
-            p.link &&
-            `
+            p.link
+              ? `
             <a href="${p.link}" class="btn white !bg-zinc-800">
             <span>자세히 보기</span>
             <span class="heroicons--arrow-long-right"></span>
           </a>
             `
+              : ''
           }
             ${
-              p.form &&
-              `
-              <a href="${p.form}" class="btn white">
+              p.form
+                ? `
+              <a href="${p.form}" target="_blank" class="btn white">
             <span>Apply Now</span>
             <span class="heroicons--arrow-long-right"></span>
           </a>
               `
+                : ''
             }
       </div>
     </div>
